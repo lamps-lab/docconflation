357fea75fc0540d60ac7d18c6eefa22e823012a8
Ryan Hiltabrand <ryhiltabrand99@gmail.com>
2022-03-04 14:41:31 -0500

    LSH

---
+++ LSH/lsh3.py
@@ -0,0 +1,57 @@
+from datasketch import MinHash, MinHashLSH
+import kshingle as ks
+#import MySQLdb as sql
+import pandas as pd
+import csv
+import datetime
+
+CSVtitle = 'Results/.9J10k256permTrueDuplicates.csv'
+def csv_w(Ocorpus, Dcourpus):
+    oringinal_corpus = Ocorpus.replace('/n', '')
+    with open(CSVtitle, mode='a') as csv_file:
+        fieldNames = ['Corpus', 'Amount' , 'Duplicates']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldNames)
+        row = f'[{str(int(oringinal_corpus))}], [{str(len(Dcourpus))}], [{str(Dcourpus)}]'
+        writer.writerow({'Corpus': f'{str(int(oringinal_corpus))}', 'Amount': f'{str(len(Dcourpus))}', 'Duplicates': f'{str(Dcourpus)}'})
+        print(row)
+        
+def csv_header():
+    with open(CSVtitle, mode='w') as csv_file:
+        fieldNames = ['Corpus' , 'Amount' , 'Duplicates']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldNames)
+
+
+csv_header()
+StartTime = datetime.datetime.now()
+df = pd.read_csv('/data/rhiltabr/s2/S2ORC_RESEARCH/DocumentConflation_Comparisons/NEWyearTrial.csv')
+
+d={}    
+with_wildcard = False
+count = 0
+lsh = MinHashLSH(threshold=0.9, num_perm=256)
+
+for index, z in df.iterrows():
+    Corpus = z['corpus_id']
+    Title = z['title']
+    s = ks.shingleset_k(Title, k = 10)
+    d["{0}".format(Corpus)] = MinHash(num_perm=256)
+    for shingle in s:
+        d["{0}".format(Corpus)].update(shingle.encode('utf8'))
+    lsh.insert(f"{Corpus}", d["{0}".format(Corpus)])
+    print(Corpus)
+#TestFile = '/data/rhiltabr/s2/S2ORC_RESEARCH/DocumentConflation_Comparisons/test.txt'
+TestFile = '/data/rhiltabr/s2/S2ORC_RESEARCH/DocumentConflation_Comparisons/known.txt'
+
+with open(TestFile) as f:
+    test = f.readlines()
+test = [x.strip() for x in test]
+for y in test:
+    results = lsh.query(d["{0}".format(y)])
+    #print(results)
+    if len(results) > 1:
+        results = [int(x) for x in results]
+        csv_w(y, results)
+
+EndTime = datetime.datetime.now()
+
+print(StartTime, EndTime)

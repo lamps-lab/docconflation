ea36ace7567455d1136075d9d6c4a1f77a088f6e
domsoos <soos.domi@gmail.com>
2022-06-02 09:23:58 -0400

    inserted keys to db

--- KeyMapping/KeyCreation.py
+++ KeyMapping/KeyCreation.py
@@ -1,28 +1,39 @@
-import xlrd
-import mysql.connector
-import sys
-import re
+"""
+Program to generate based on paper 
 
-#Add the proper connection information depending on the server you are on
-db = mysql.connector.connect(
-    host = "", #hawking.cs.odu.edu is the server i think you need
-    user = "", #sql user name
-    passwd = "", #sql password
-    database = "" #database name for citeseer
-)
+@author: Ryan Hiltabrand, Dominik Soos
+"""
 
 
+# Imports
+import sys, csv, xlrd, re
+import MySQLdb as sql
 
-#I used this sql connector later in research but you will have to change code bellow so stick with above connector
-#db=sql.connect(host = "", user = "", passwd = "", db = "", charset = "utf8")
+import config
+
 
 def main():
-    #the two arguments you need are for the loop, I believe I had these set as I had to run into issues but essentially its just the size of the database
-    startValue = int(sys.argv[1])
-    endValue = int(sys.argv[2]) 
 
-    mycursor = db.cursor()
+    # Add the proper connection information depending on the server you are on
+    db = sql.connect(
+        host = config.hawkingh,
+        user = config.hawkingu,
+        passwd = config.hawkingp,
+        db = config.hawkingdb,
+        charset = "utf8"
+    )
+    hawking = db.cursor()
 
+    db = sql.connect(
+        host = config.chandrah,
+        user = config.chandrau,
+        passwd = config.chandrap,
+        db = config.chandradb,
+        charset = "utf8"
+    )
+    chandra = db.cursor()
+
+    # init
     first = ''
     second = ''
     third = ''
@@ -30,36 +41,61 @@ def main():
     fifth = ''
     sixth = ''
 
-    for i in range (startValue, endValue+1):
+    # Getting the input file from Trial.csv that we retrieved from citeseerx database
+    # The input file has the format id, title, year
+    data = csv.reader(open("./Trial.csv"))#, index_col=0)
+
+    # skip the header of the csv file
+    next(data)
+
+    i = 0
+    # loop through each row of input file
+    for row in data:
+        i += 1
+
+        #print("id", row[0], "title", row[1], "year", row[2])
+        print("\n\nid:", row[0] ,"original title: ", row[1])
         
-        #Change code below to the right sql for database name and title creation
-        cur = f"SELECT database_id, corpus_id, title, authors, year FROM s2orcDATA WHERE database_id = {i}"
-        mycursor.execute(cur)
+        paperid = row[0]
+        title = row[1].lower()
+        year = row[2]
 
-        current =  mycursor.fetchone()
-        databaseid = current[0]
-        paperid = current[1]
-        alphaNumericTitle = current[2].lower()
+        # get all the authors as a tuple
+        auth = ("SELECT name FROM authors WHERE paperid = '%s'" % (paperid))
+        hawking.execute(auth)
+        authors = hawking.fetchall()
 
-        alphaNumericTitle = alphaNumericTitle.replace("[", '')
-        alphaNumericTitle = alphaNumericTitle.replace("]", '')
-        alphaNumericTitle = alphaNumericTitle.replace(":", '')
-        alphaNumericTitle = alphaNumericTitle.replace("'", '')
-        alphaNumericTitle = alphaNumericTitle.replace('"', '')
-        alphaNumericTitle = alphaNumericTitle.replace(';', '')
-        alphaNumericTitle = alphaNumericTitle.replace('{', '')
-        alphaNumericTitle = alphaNumericTitle.replace('}', '')
-        alphaNumericTitle = alphaNumericTitle.replace('!', '')
-        alphaNumericTitle = alphaNumericTitle.replace('.', '')
-        alphaNumericTitle = alphaNumericTitle.replace(',', '')
-
-        firstAuthor = firstA(current[3])
-        lastAuthor = last(current[3])
-        year = current[4]
-
-        titleList = alphaNumericTitle.split()
+        if len(authors) > 0:
+            firstAuthor = str(authors[0])
+        else:
+            firstAuthor = None
+        
+        if len(authors) > 1:
+            secondAuthor = str(authors[1])
+        else:
+            secondAuthor = None
 
 
+        # could make it to a function?
+        # regular expression to get only alphanumerical array of words of authors and title
+        # if any of the str is alphanumerical, its returned as list
+        pattern = [r'\w+']
+        for p in pattern:
+            match = re.findall(p, title)
+            titleList = match
+
+            if firstAuthor:
+                match = re.findall(p, firstAuthor)
+                # get the last name of the first author
+                firstAuthor = str(match[-1])
+            if secondAuthor:
+                match = re.findall(p, secondAuthor)
+                # get the last name of the second author
+                secondAuthor = str(match[-1])
+
+        print(firstAuthor, secondAuthor)
+
+        
         if len(titleList) > 5:
             first = longestString(titleList)
             titleList.remove(first)
@@ -137,23 +173,43 @@ def main():
 
         four_firstKey = FourkeyString(first, second, third, forth, firstAuthor)
         four_secondKey = FourkeyString(second, third, forth, fifth, firstAuthor)
-        four_thirdKey = FourkeyString(first, second, third, forth, lastAuthor)
-        four_forthKey = FourkeyString(second, third, forth, fifth, lastAuthor)
+        four_thirdKey = FourkeyString(first, second, third, forth, secondAuthor)
+        four_forthKey = FourkeyString(second, third, forth, fifth, secondAuthor)
+
+        print()
+        print("Four\n first:", four_firstKey, "\nsecond:", four_secondKey, "\nthird:", four_thirdKey, "\nfourth:", four_forthKey)
+        
 
         five_firstKey = FivekeyString(first, second, third, forth, fifth, firstAuthor)
         five_secondKey = FivekeyString(second, third, forth, fifth, sixth, firstAuthor)
-        five_thirdKey = FivekeyString(first, second, third, forth, fifth, lastAuthor)
-        five_forthKey = FivekeyString(second, third, forth, fifth, sixth, lastAuthor)
+        five_thirdKey = FivekeyString(first, second, third, forth, fifth, secondAuthor)
+        five_forthKey = FivekeyString(second, third, forth, fifth, sixth, secondAuthor)
 
-        print(i)
-        mycursor.execute("INSERT INTO FourWordsKey (database_id, corpus_id, key1, key2, key3, key4, year) VALUES(%s, %s, %s, %s, %s, %s,%s)", (databaseid, paperid, four_firstKey, four_secondKey, four_thirdKey, four_forthKey, year))
+
+        print("\nFive\n first:", five_firstKey, "\nsecond:", five_secondKey, "\nthird:", five_thirdKey, "\nfourth:", five_forthKey)
+        print()
+        print()
+
+        if year:
+            chandra.execute("INSERT INTO FourWordsKey (corpus_id, key1, key2, key3, key4, year) VALUES(%s, %s, %s, %s, %s,%s)", (paperid, four_firstKey, four_secondKey, four_thirdKey, four_forthKey, year))
+        else:
+            chandra.execute("INSERT INTO FourWordsKey (corpus_id, key1, key2, key3, key4) VALUES(%s, %s, %s, %s, %s)", (paperid, four_firstKey, four_secondKey, four_thirdKey, four_forthKey))
         db.commit()
-        mycursor.execute("INSERT INTO FiveWordsKey (database_id, corpus_id, key1, key2, key3, key4, year) VALUES(%s, %s, %s, %s, %s, %s,%s)", (databaseid, paperid, five_firstKey, five_secondKey, five_thirdKey, five_forthKey, year))
+
+        if year:
+            chandra.execute("INSERT INTO FiveWordsKey (corpus_id, key1, key2, key3, key4, year) VALUES(%s, %s, %s, %s, %s,%s)", (paperid, five_firstKey, five_secondKey, five_thirdKey, five_forthKey, year))
+        else:
+            chandra.execute("INSERT INTO FiveWordsKey (corpus_id, key1, key2, key3, key4) VALUES(%s, %s, %s, %s, %s)", (paperid, five_firstKey, five_secondKey, five_thirdKey, five_forthKey))
         db.commit()
+        
+    print(i)
+        
+
 
 def longestString(List):
     longest_string = max(List, key=len)
     return longest_string
+
 
 def keyString(one, two, three,four):
     if four == None:
@@ -168,7 +224,7 @@ def FourkeyString(one, two, three, four, author):
         key = one+'_'+two+'_'+three+'_'+four
         return key
     else:
-        key = one+'_'+two+'_'+three+'_'+four+'_'+author.lower()
+        key = one+'_'+two+'_'+three+'_'+four+'_'+author
         return key
 
 def FivekeyString(one, two, three, four, five, author):
@@ -176,7 +232,7 @@ def FivekeyString(one, two, three, four, five, author):
         key = one+'_'+two+'_'+three+'_'+four+'_'+five
         return key
     else:
-        key = one+'_'+two+'_'+three+'_'+four+'_'+five+'_'+author.lower()
+        key = one+'_'+two+'_'+three+'_'+four+'_'+five+'_'+author
         return key
 
 def firstA(authors):
---
+++ KeyMapping/config.py
@@ -0,0 +1,10 @@
+
+chandrah = "chandra.cs.odu.edu" #chandra.cs.odu.edu is the server i think you need
+chandrau = "domsoos" #sql user name
+chandrap = "FriMay64:01:22PM" #sql password
+chandradb = "citeseerx_nearduplicate_data" #database name for citeseer
+
+hawkingh = "hawking.cs.odu.edu"
+hawkingu = "domsoos"
+hawkingp = "FriFeb184:17PM"
+hawkingdb = "citeseerx"
